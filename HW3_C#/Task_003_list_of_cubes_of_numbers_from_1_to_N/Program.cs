bab4e6a0a730c2940810974745a07e41a4175b72
Aleksandr Lovygin <Shurik.lovygin@gmail.com>
2023-06-02 22:42:29 +0300

    Problem not solved

---
+++ HW3_C#/Task_003_list_of_cubes_of_numbers_from_1_to_N/Program.cs
@@ -0,0 +1,11 @@
+﻿//Написать список кубов чисел от 1 до N, где N  введенное с консоли число.
+
+Console.Clear();
+Console.WriteLine("Please, enter integer value");
+int N = int.Parse(Console.ReadLine());
+int[] cubes = new int[N];
+    for(int i = 1; i <= N; i++)
+    {
+        cubes[i-1] = (int)Math.Pow(i, 3);//Нужно написать (int) перед Math.Pow(...) ХЗ зачем и когда конкретно.
+    }
+    Console.WriteLine(cubes);
